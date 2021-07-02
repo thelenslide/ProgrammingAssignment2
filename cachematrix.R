da5a6b66f34661b5babe5059d747e0baba147966
thelenslide <86812000+thelenslide@users.noreply.github.com>
2021-07-02 10:15:25 +0800

    trialcommit
    Hi I am a beginner so I am testing this part i dont know what to do
    
    Co-Authored-By: Roger D. Peng <9612+rdpeng@users.noreply.github.com>

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mtrx <- NULL
+  set <- function(y) {
+    x <<- y
+    mtrx <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) mtrx <<- inverse
+  getinverse <- function() mtrx
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  mtrx <- x$getinverse()
+  if(!is.null(mtrx)) {
+    message("getting cache inverse data")
+    return(mtrx)
+  }
+  data <- x$get()
+  mtrx <- solve(data, ...)
+  x$setinverse(mtrx)
+  mtrx
 }
